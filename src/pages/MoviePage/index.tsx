40d039825e019f68f4e73d169496ebb7d2a1420f
beeek3r <baker.xu39@gmail.com>
2019-10-23 19:13:45 +1000

    Movies page added movie poster img and movie info

--- src/pages/MoviePage/index.tsx
+++ src/pages/MoviePage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {FETCH_POPULAR_MOVIES_CREATOR, QUERY_MOVIES, QUERY_MOVIES_CREATOR} from '../../redux/actions';
+import moment from 'moment';
 
 class MoviePage extends React.Component <any> {
   constructor(props: any) {
@@ -13,15 +14,43 @@ class MoviePage extends React.Component <any> {
     }
   }
 
+
+
   render() {
+
+    let reviewScore = Math.floor(this.props.selectedMovie.vote_average * 10) + "%";
+    let releaseDate = moment(this.props.selectedMovie).format('MMMM YYYY');
+    let totalVotes = this.props.selectedMovie.vote_count;
+
+    console.log(this.props.selectedMovie, 'From inside movie page');
     return (
       <div className="App">
           <div className="Movie-background-img" style={{backgroundImage: `url("https://image.tmdb.org/t/p/original${this.props.selectedMovie.backdrop_path}")`}}></div>
+          
           <i className="Movie-back-button fas fa-arrow-left" onClick={() => this.props.history.push('/')}/>
-          <div className="Movie-info">
-              <h1>{this.props.selectedMovie.title}</h1>
-              <p>{this.props.selectedMovie.overview}</p>
+          <div className="Movie-info" style={{display: 'flex'}}>
+
+            {/* {Don't add extra CSS flex, it screws it} */}
+            <div className="Movie-page-img-wrapper">
+              <div className="Movie-page-img" style={{backgroundImage: `url("https://image.tmdb.org/t/p/w500/${this.props.selectedMovie.poster_path}")`}}></div>
+            </div>
+  
+            <div className="Movie-page-info-wrapper">
+              <h2>{this.props.selectedMovie.title}</h2>
+              <p className="Movie-page-movie-info">
+                Release: {releaseDate} <br />
+                User Score: {reviewScore} <br />
+                Votes: {totalVotes} 
+              </p>
+            </div>
           </div>
+
+          <div className="Movie-page-overview">
+              <h3>Overview</h3>
+              <p className="Movie-page-movie-info">{this.props.selectedMovie.overview}</p>
+          </div>
+
+
       </div>
     )
   }
