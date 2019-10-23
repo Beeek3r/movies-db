eff1d2206fb749acb101641ae00ebc3ff646a4b1
beeek3r <baker.xu39@gmail.com>
2019-10-23 21:36:04 +1000

    added movie % onto movie tile

--- src/components/MovieItem/index.tsx
+++ src/components/MovieItem/index.tsx
@@ -4,23 +4,36 @@ import UnavaliablePoster from '../../assets/unavailable.png'
 import moment from 'moment';
 
 const MoviesItem = (props: MovieItemInterface) => {
+    const {id,original_title, poster_path} = props.movieInfo;
     let releaseDate = moment(props.movieInfo.release_date).format('MMMM YYYY');
+    let score = Math.floor(props.movieInfo.vote_average * 10);
+
+    // User clicks on movie tile
+    let selectMovie = () => {
+        props.history.push(`/movie/${id}`);
+        props.selectMovie(props.movieInfo);
+    }
 
     return (
-        <div className="Movie-tile" onClick={()=> {
-            props.history.push(`/movie/${props.movieInfo.id}`);
-            props.selectMovie(props.movieInfo);
-        }}>
+        <div className="Movie-tile" onClick={()=> {selectMovie()}}>
 
             {props.movieInfo.poster_path ? 
-            <img className="Movie-image" src={`https://image.tmdb.org/t/p/w500/${props.movieInfo.poster_path}`} />
+            <img className="Movie-image" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} />
             :
             <img className="Movie-image" src={UnavaliablePoster} />}
 
-            <p className="Movie-title">{props.movieInfo.original_title}</p>
+            <div className={`Movie-upvote ${scoreColour(score)}`}>{score}%</div>
+
+            <p className="Movie-title">{original_title}</p>
             <p className="Movie-date">{releaseDate}</p>
         </div>
     )
+}
+
+let scoreColour = (score:number) => {
+    if (score < 50) return "Movie-upvote-low"
+    if (score < 75 && score > 50)  return "Movie-upvote-med"
+    if (score > 75) return "Movie-upvote-high"
 }
 
 export default MoviesItem;
